6c7d8f5bc744ba6d153d5377f473f11ce9e1652d
matos795 <matosalexandre795@gmail.com>
2025-12-05 13:39:13 -0300

    New Component Header, Put Header&Outlet in Home-index

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Header() {
+    return(
+        <header>
+            <nav className='rt-container'>
+                <h2>MeuSite</h2>
+            </nav>
+        </header>
+    );
+}
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,12 @@
+import Header from "../../components/Header";
+import { Outlet } from "react-router-dom";
 
 
 export default function Home() {
     return(
         <>
-            
+            <Header />
+            <Outlet />
         </>
     );
 }
